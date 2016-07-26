aa123c831f3ee787ceeb0add92123c56a55719c6
Ritesh Shakya <riteshakya037@gmail.com>
2016-07-26 23:38:40 +0545

    Changed Details panel to sliding instead of dialog

--- app/src/main/java/com/blues/gallery/Activity/CarouselActivity.java
+++ app/src/main/java/com/blues/gallery/Activity/CarouselActivity.java
@@ -3,25 +3,40 @@ package com.blues.gallery.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.Transformation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.blues.gallery.Adaptors.ImageModel;
 import com.blues.gallery.BottomSheet.BottomSheet;
 import com.blues.gallery.CustomViews.MultiViewPager;
 import com.blues.gallery.EventHandlers.ZoomOutPageTransformer;
+import com.blues.gallery.Helper.Utils;
 import com.blues.gallery.R;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-public class CarouselActivity extends AppCompatActivity implements View.OnClickListener {
+public class CarouselActivity extends AppCompatActivity implements View.OnClickListener, ViewPager.OnPageChangeListener {
     public ArrayList<ImageModel> data = new ArrayList<>();
     int pos;
     MultiViewPager pager;
 
     private BottomSheet bottomSheet;
+    LinearLayout footer;
+    RelativeLayout imageView;
+    ImageView threeImage;
+    private int oldPosition = Integer.MAX_VALUE;
 
 
     @Override
@@ -50,20 +65,112 @@ public class CarouselActivity extends AppCompatActivity implements View.OnClickL
         pager.setAdapter(adapter);
         pager.setPageTransformer(true, new ZoomOutPageTransformer());
         pager.setCurrentItem(pos);
-        RelativeLayout imageView = (RelativeLayout) findViewById(R.id.threeButton);
+        pager.addOnPageChangeListener(this);
+        imageView = (RelativeLayout) findViewById(R.id.threeButton);
         imageView.setOnClickListener(this);
+        threeImage = (ImageView) findViewById(R.id.threeImage);
+
+        footer = (LinearLayout) findViewById(R.id.contentPanel);
     }
+
 
     @Override
     public void onClick(View view) {
-        showBottomSheet(pager.getCurrentItem());
+        if (footer.getVisibility() == View.VISIBLE) {
+            collapse(footer);
+        } else {
+            expand(footer);
+        }
+
+//        showBottomSheet(pager.getCurrentItem());
     }
 
-    public void showBottomSheet(int mPageNumber) {
-        if (bottomSheet == null || !bottomSheet.isShowing()) {
-            bottomSheet = new BottomSheet(this, data.get(mPageNumber));
-            bottomSheet.show();
+    public void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        RotateAnimation animation = new RotateAnimation(0.0f, 180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        animation.setFillAfter(true);
+        threeImage.startAnimation(animation);
+        // 1dp/ms
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public void expand(final View v) {
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? LinearLayout.LayoutParams.WRAP_CONTENT
+                        : (int) (targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+        RotateAnimation animation = new RotateAnimation(180f, 360.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        threeImage.startAnimation(animation);
+        // 1dp/ms
+        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (oldPosition != position) {
+            oldPosition = position;
+
+            TextView title = (TextView) findViewById(R.id.photoTitle);
+            title.setText(data.get(position).getName());
+
+            File file = new File(data.get(position).getUrl());
+            Date d = new Date(file.lastModified());
+            String PATTERN = "MMMM dd, yyyy";
+            SimpleDateFormat dateFormat = new SimpleDateFormat();
+            dateFormat.applyPattern(PATTERN);
+
+            TextView dateTaken = (TextView) findViewById(R.id.dateTaken);
+            dateTaken.setText(dateFormat.format(d));
+
+            TextView albumName = (TextView) findViewById(R.id.albumName);
+            albumName.setText(Utils.getName(file.getParent()));
         }
     }
 
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
